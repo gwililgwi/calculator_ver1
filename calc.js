25b91686d3463edee5af135f9e12f2c400130eb0
gwililgwi <80652061+gwililgwi@users.noreply.github.com>
2021-05-07 04:58:48 +0900

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,15 @@
+var tx1=document.getElementById('tx1');
+var tx2=document.getElementById('tx2');
+
+function calc1(){
+    return console.log(parseInt(tx1.value)+parseInt(tx2.value));
+}
+function calc2(){
+    return console.log(parseInt(tx1.value)-parseInt(tx2.value));
+}
+function calc3(){
+    return console.log(parseInt(tx1.value)*parseInt(tx2.value));
+}
+function calc4(){
+    return console.log(parseInt(tx1.value)/parseInt(tx2.value));
+}
